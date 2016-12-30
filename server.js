63e61717ba20d4269cc9c1e1bfac9c883403d787
ThisisDennis <dennis.hinterwimmer@gmail.com>
2016-12-30 15:01:34 +0100

    commiting

--- server.js
+++ server.js
@@ -1,30 +1,3 @@
-/*var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-users = [];
-connections =[];
-
-server.listen(process.env.PORT || 3000);
-console.log('Server running...');
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.sockets.on('connection', function(socket){
-  connections.push(socket);
-  console.log('connected: %s sockets connected', connections.length);
-
-  // Disconnect
-  socket.on('disconnect', function(data){
-    connections.splice(connections.indexOf(socket),1);
-    console.log('Disconnected: %s sockets connected', connections.length);
-  });
-  socket.on('send message', function(data){
-    io.sockets.emit('new message', {msg:data});
-  })
-}) */
 
 var app=require ('express')();
 var http= require('http').Server(app);
@@ -53,6 +26,6 @@ socket.on('obj',function(data){
     console.log('one user disconnected' + socket.id);
   })
 })
-http.listen(80,function(){
-  console.log('server listening on port 80');
+http.listen(8080,function(){
+  console.log('server listening on port 8080');
 });
